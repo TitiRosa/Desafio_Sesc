e84aad9a79dbb404889801e3c069725ddf2bdaf8
Leticia Rosa <leticiamartins187@hotmail.com>
2025-08-18 01:08:05 -0300

    inserido mais um teste que valida criar usuario com cpf invalido

--- src/test/java/tests/AtualizarLoginTest.java
+++ src/test/java/tests/AtualizarLoginTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import jdk.jfr.Description;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.UserManagementPage;
@@ -8,7 +9,8 @@ import pages.UserManagementPage;
 
 public class AtualizarLoginTest extends BaseTest {
 
-   @Test
+    @Test(priority = 1)
+    @Description("Editar Email")
     public void testEditarEmailUsuario() {
         HomePage homePage = new HomePage(driver);
         homePage.clicarConfigMenu();
@@ -23,5 +25,23 @@ public class AtualizarLoginTest extends BaseTest {
 
         String novoEmail = faker.internet().emailAddress();
         userPage.editarEmailUsuarioPorNome(nomePesquisa, novoEmail);
+    }
+
+    @Test(priority = 2)
+    @Description("Editar Senha")
+    public void testEditarSenhaUsuario() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clicarConfigMenu();
+        homePage.clicarGerenciamentoUsuarios();
+        homePage.clicarBuscarUsuário();
+
+        UserManagementPage userPage = new UserManagementPage(driver);
+        userPage.selecionarEmpresa("SENAC");
+
+        String nomePesquisa = "Teste07";
+        userPage.pesquisarPorNome(nomePesquisa);
+
+        String senha = faker.internet().password(5, 10, true, true, true);
+        userPage.editarEmailUsuarioPorNome(nomePesquisa, senha);
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,10 +1,8 @@
 package tests;
 
 import base.BaseTest;
-import base.DadosUsuario;
 import jdk.jfr.Description;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -126,6 +124,68 @@ public class LoginTest extends BaseTest {
 @Test (priority = 4)
 @Description("Validar que o usuário não pode ser criado sem Email")
  public void testInserirUsuárioSemEmail() {
+        String nomeCompleto = faker.name().fullName();
+        String cpf = faker.number().digits(4);
+        String nomeUsuario = faker.name().username();
+        String email = faker.internet().emailAddress();
+        String senha = faker.internet().password(5, 10, true, true, true);
+        String poloValue = "6993";
+        String empresa = "SENAC";
+        String uf = "Rio Grande do Sul (RS)";
+        String perfil = "Gerenciar - Teste prático QA";
+        String polo = "TESTE CANDIDATO 2";
+
+
+        HomePage homePage = new HomePage(driver);
+        homePage.clicarConfigMenu();
+        homePage.clicarGerenciamentoUsuarios();
+        homePage.clicarCadastrar();
+
+       UserManagementPage userPage = new UserManagementPage(driver);
+          userPage.createUserWithCompany(
+                nomeCompleto,
+                email,
+                cpf,
+                nomeUsuario,
+                senha,
+                empresa,
+                uf,
+                perfil,
+                polo,
+                poloValue
+        );
+
+      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+    try {
+        By errorMessageLocator = By.xpath("//div[contains(text(), 'CPF inválido')]");
+
+        WebElement errorMessageToast = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessageLocator));
+
+        String actualErrorMessage = errorMessageToast.getText().replace("\n", " ").trim();
+        String expectedErrorMessage = "Erro:CPF inválido";
+
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, "A mensagem de erro não corresponde ao esperado");
+        System.out.println("VALIDAÇÃO: A mensagem de erro está correta.");
+
+    } catch (Exception e) {
+        System.out.println("FALHA: O toast de erro não foi exibido ou a mensagem está incorreta.");
+        throw e;
+    }
+    try {
+        WebElement successMessageToast = wait.until(ExpectedConditions.visibilityOfElementLocated(userPage.getSuccessMessageLocator()));
+        String actualSuccessMessage = successMessageToast.getText().replace("\n", " ").trim();
+        String expectedSuccessMessage = "Escolha um perfil";
+        Assert.assertEquals(actualSuccessMessage, expectedSuccessMessage, "A mensagem de sucesso não corresponde ao esperado.");
+        System.out.println("VALIDAÇÃO: A mensagem de sucesso também apareceu, indicando um possível bug.");
+    } catch (Exception e) {
+        System.out.println("A mensagem de sucesso não foi encontrada, o que é o comportamento esperado para este teste de falha.");
+    }
+ }
+
+ @Test (priority = 5)
+@Description("Validar que o usuário não ter CPF com menos dígitos")
+ public void testInserirUsuárioComCpfInvalido() {
         String nomeCompleto = faker.name().fullName();
         String cpf = faker.number().digits(11);
         String nomeUsuario = faker.name().username();
